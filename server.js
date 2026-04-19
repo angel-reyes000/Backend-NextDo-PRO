116ca9b6cfba8505c1070bb41a1f5a9fa31424b4
Angel Reyes <ar731684@gmail.com>
2026-04-19 00:30:43 -0600

    origins *

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: 'https://nextdo-pro.vercel.app'
+    origin: '*'
 }))
 
 app.get('/', (req, res) => {
